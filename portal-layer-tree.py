e641172475066138cfb43cf71bc1760b5de0d4e5
Fernando González <fernando.gonzalez@geomati.co>
2014-03-07 12:09:21 +0100

    Layer tree now shows the map layers of a portal layer between brackets. It also shows the map layer order below the tree

--- portal-layer-tree.py
+++ portal-layer-tree.py
@@ -3,14 +3,17 @@
 
 import json
 import argparse
+import portal
 
-def showGroupItems(items, indent):
+def showGroupItems(root, items, indent):
   for item in items:
     if isinstance(item, basestring):
-      print('\t' * indent + item)
+      portalLayer = portal.findLayerById(root["portalLayers"], item)
+      mapLayers = portalLayer["layers"]
+      print('\t' * indent + item + " (" + ", ".join(mapLayers) + ")")
     else:
       print('\t' * indent + item.get("id"))
-      showGroupItems(item.get("items"), indent+1)
+      showGroupItems(root, item.get("items"), indent+1)
 
 parser=argparse.ArgumentParser(description='''Muestra el árbol de capas especificado en el fichero layers.json.''')
 parser.add_argument("--file", help="Ruta al fichero layers.json file (por defecto /var/portal/layers.json)", default="/var/portal/layers.json", nargs='?')
@@ -19,6 +22,17 @@ args=parser.parse_args()
 with open(args.file, "r") as file:
   root=json.load(file)
 
-print "root"
-showGroupItems(root["groups"], 1)
+print "LAYER TREE"
+print "=========="
 
+print "root"
+showGroupItems(root, root["groups"], 1)
+
+mapLayers = filter(lambda layer : layer["visible"], root["wmsLayers"])
+ids = map(lambda layer : layer["id"], mapLayers)
+
+print "\n"
+print "MAP LAYER ORDER"
+print "==============="
+for i in range(len(ids)):
+  print str(i + 1) + ". " + ids[i]
