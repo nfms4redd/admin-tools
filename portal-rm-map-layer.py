09e78b7ff450272c453e8e8725f74dac425e826c
Fernando González <fernando.gonzalez@geomati.co>
2014-03-12 10:49:34 +0100

    Add specific scripts to manage map layers (wms layers) individually

---
+++ portal-rm-map-layer.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import portal
+import argparse
+
+parser = argparse.ArgumentParser(description='Elimina una capa de mapa del fichero layers.json')
+parser.add_argument("--file", help="Ruta al fichero layers.json file (por defecto /var/portal/layers.json)", default="/var/portal/layers.json", nargs='?')
+parser.add_argument('--id', help="Identificador de la capa a eliminar", required=True)
+args = parser.parse_args()
+
+args = parser.parse_args()
+
+root = portal.readPortalRoot(args.file)
+
+mapLayer = portal.findLayerById(root["wmsLayers"], args.id)
+if not mapLayer:
+    print "No se ha podido encontrar la capa de mapa: " + args.id
+    exit(1)
+
+portalLayers = root["portalLayers"]
+
+for portalLayer in portalLayers:
+  layers = portalLayer["layers"]
+  if args.id in layers:
+    if len(layers) > 1:
+      layers.remove(args.id)
+    else:
+      print "No se puede eliminar la capa de mapa. La capa de portal '" + portalLayer.id + "' no tiene otra capa de mapa asociada."
+      exit(1)
+
+root["wmsLayers"].remove(mapLayer)
+
+portal.writePortalRoot(root, args.file)
---
+++ portal-set-map-layer.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse
+import portal
+
+parser = argparse.ArgumentParser(description='Modifica una capa en el fichero layers.json')
+parser.add_argument("--file", help="Ruta al fichero layers.json file (por defecto /var/portal/layers.json)", default="/var/portal/layers.json", nargs='?')
+parser.add_argument('--id', help="Identificador de la capa a modificar", required=True)
+parser.add_argument('--url', help="Nueva URL de la capa de GeoServer")
+parser.add_argument('--wmsName', help="Nuevo nombre de la capa en GeoServer")
+parser.add_argument('--wmsTime', help="Cadena que contiene los diferentes instantes temporales que se pueden pedir a GeoServer")
+parser.add_argument('--imageFormat', help="Formato de la imagen que se pedirá a GeoServer")
+parser.add_argument('--sourceLabel', help="Etiqueta de la fuente de datos")
+parser.add_argument('--sourceLink', help="Enlace informativo a la fuente de datos")
+parser.add_argument('--label', help="Nueva etiqueta de la capa")
+parser.add_argument('--legend', help="Imagen que contiene la leyenda de la capa. Es necesario especificar el idioma de la leyenda (--lang)")
+parser.add_argument('--lang', help="Idioma de la leyenda o fichero de información que se quiere modificar.")
+parser.add_argument('--queryable', help="Hace que la capa se pueda consultar", action="store_true")
+parser.add_argument('--not-queryable', help="Hace que la capa no se pueda consultar", action="store_true")
+parser.add_argument('--hidden', help="Hace que la capa no se muestre nunca ni en el mapa ni en el árbol de capas", action="store_true")
+parser.add_argument('--not-hidden', help="Hace que la capa se pueda mostrar y ocultar", action="store_true")
+
+args = parser.parse_args()
+
+portal.checkMapLayerArgs(args)
+
+root = portal.readPortalRoot(args.file)
+mapLayer = portal.findLayerById(root["wmsLayers"], args.id)
+
+if mapLayer is None:
+  print "No se encuentra la capa: " + args.id
+  exit(1)
+
+#Find portal layer
+portalLayer = None
+for layer in root["portalLayers"]:
+  if args.id in layer["layers"]:
+    portalLayer = layer
+    break
+
+mapLayer = portal.updateMapLayer(portalLayer, mapLayer, args)
+portal.writePortalRoot(root, args.file)
+
