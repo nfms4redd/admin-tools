55277bcfcfcbd138289812a53a527de705e50075
Víctor González <victor.gonzalez@geomati.co>
2018-02-26 14:29:07 +0100

    Remove unused files

--- layers_json/default.sh
+++
@@ -1,8 +0,0 @@
-schema=nfms
-encoding=UTF8
-databasename=geoserverdata
-files=*.shp
-host=localhost
-port=5432
-workspace=nfms
-datastore=geoserverdata
--- layers_json/pg-parse.sh
+++
@@ -1,8 +0,0 @@
-source parse.sh
-
-if [ ! -v crs ]
-then
-	echo "El sistema de referencia de coordenadas del fichero debe ser especificado. Ejemplo: --crs 4326"
-	exit 1 
-fi
-
